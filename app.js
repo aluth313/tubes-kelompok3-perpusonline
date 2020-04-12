d67102399555fa885af40d49b87d0a2cf2d9daa3
Muhammad Taufik <muhamadtauf@gmail.com>
2020-04-12 20:50:37 +0700

    revisi sidebar admin

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
+//fitur yang digunakan pada perangkat lunak
 var express = require("express");
 var path = require("path");
-
 var app = express();
-
 app.use(express.static('public'));
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//routing
 app.get("/", function(request, response) {
 	response.render('sites/index');
 });
 
-app.get("/admin", function(request, response) {
-	response.render('sites/admin/dashboard');
+//routing
+app.get('/admin/dashboard', (req, res) => {
+	//variable yang akan dikirim ke view
+	var buku = 20;
+	var ebook = 25;
+	var anggota = 30;
+	var pendaftar = 35;
+
+	//mengirim variable ke view
+	res.render('sites/admin/dashboard', {
+		buku : buku,
+		ebook : ebook,
+		anggota : anggota,
+		pendaftar : pendaftar
+	});
 });
 
+//port
 app.listen(3000, () => {
 	console.log('Server Started');
 });
