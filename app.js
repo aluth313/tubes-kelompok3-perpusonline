92e17782fe834cb6f28c00127bf5ca77f2f30b15
Gilang Krisma Wijaya <gilangkrisma52@gmail.com>
2020-04-12 22:26:49 +0700

    Gilang nyoba commit

--- app.js
+++ app.js
@@ -7,12 +7,41 @@ app.use(express.static('public'));
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
 	response.render('sites/index');
 });
 
-app.get("/admin", function(request, response) {
-	response.render('sites/admin/dashboard');
+app.get('/admin/dashboard', function (req, res) {
+	var buku = 20;
+	var ebook = 25;
+	var anggota = 30;
+	var peminjaman = 35;
+
+	res.render('sites/admin/dashboard', {
+
+		buku: buku,
+		ebook: ebook,
+		anggota: anggota,
+		peminjaman: peminjaman
+	});
+
+});
+
+app.get('/admin/master/buku', function (req, res) {
+	res.render('sites/admin/master/buku')
+
+});
+app.get('/admin/master/anggota', function (req, res) {
+	res.render('sites/admin/master/anggota')
+
+});
+app.get('/admin/master/ebook', function (req, res) {
+	res.render('sites/admin/master/ebook')
+
+});
+app.get('/admin/master/peminjaman', function (req, res) {
+	res.render('sites/admin/master/peminjaman')
+
 });
 
 app.listen(3000, () => {
